6540a51cc458d4d6d4d090d7c6ed44cf846718cb
lily4178993 <nellytelli@gmail.com>
2023-11-28 22:27:28 +0000

    feat: implement Genre class
    - Add Genre class with unique ID generation.
    - Include methods for item management and bidirectional relationships.
    - Utilize SecureRandom for UUID generation.

---
+++ classes/genre.rb
@@ -0,0 +1,27 @@
+require 'securerandom'
+require_relative 'item'
+
+class Genre
+  attr_accessor :name, :items
+  attr_reader :id
+
+  # Initialize a new Genre instance with a unique ID and an empty collection of items.
+  def initialize(name)
+    @id = generate_uuid
+    @name = name
+    @items = []
+  end
+
+  # Add an item to the genre's collection, establishing a bidirectional relationship.
+  def add_item(item)
+    @items << item unless @items.include?(item)
+    item.genre = self
+  end
+
+  private
+
+  # Generate a unique UUID to be used as the genre's identifier.
+  def generate_uuid
+    SecureRandom.uuid
+  end
+end
