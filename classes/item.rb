449315f4f0c6c083667bcd4c7eef3ea97fa3a5e8
richie1988 <richiessikaonga@gmail.com>
2023-11-28 18:05:45 +0200

    Created the Entry point for main file

--- classes/item.rb
+++ classes/item.rb
@@ -1,19 +1,19 @@
-# class Item
-# attr_accessor :id, :genre, :author, :label, :source, :publish_date, :archived
+class Item
+  attr_accessor :id, :genre, :author, :label, :source, :published_date, :archived
 
-#   def initialize(published_date, archived: false)
-#     @id = Random.rand(1..1000)
-#     @published_date = published_date
-#     @archived = archived
-#   end
+  def initialize(published_date, archived: false)
+    @id = Random.rand(1..1000)
+    @published_date = published_date
+    @archived = archived
+  end
 
-#   def move_to_archived
-#     @archived = true if can_be_archive?
-#   end
+  def move_to_archive
+    @archived = true if can_be_archived?
+  end
 
-#   private
+  private
 
-#   def can_be_archive?
-#     @publish_date > 10.years.ago
-#   end
-# end
+  def can_be_archived?
+    Time.now - @published_date > 10.years.ago
+  end
+end
--- main.rb
+++ main.rb
@@ -1,1 +1,13 @@
 # main.rb
+
+require_relative 'Classes/item'
+
+def print_menu
+  puts '1 Move item to archive'
+  puts '2 Quit'
+end
+
+def get_user_input(prompt)
+  print prompt
+  gets.chomp
+end
