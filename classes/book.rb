7c6d1e1f35d10965f7cbd517b0ce87320f6646c6
richie1988 <richiessikaonga@gmail.com>
2023-11-29 13:50:22 +0200

    Created the labe and book classes

--- classes/book.rb
+++ classes/book.rb
@@ -1,1 +1,15 @@
 # book.rb
+require_relative 'item'
+
+class Book < Item
+  attr_accessor :cover_state
+
+  def initialize(genre, publish_date, cover_state)
+    super(genre, publish_date)
+    @cover_state = cover_state
+  end
+
+  def can_be_archived?
+    super || @cover_state == 'bad'
+  end
+end
--- classes/label.rb
+++ classes/label.rb
@@ -1,1 +1,16 @@
 # label.rb
+require_relative 'item'
+
+class Label
+  attr_accessor :name, :items
+
+  def initialize(name)
+    @name = name
+    @item = []
+  end
+
+  def add_item(item)
+    @items << item
+    item.set.label(self)
+  end
+end
